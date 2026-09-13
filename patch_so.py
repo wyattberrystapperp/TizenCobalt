457ad79101b1d7f42c5bdbc3c7d3ccad9d7f6e55
wyattberrystapperp <wyatt@example.com>
2026-09-13 11:05:43 +0000

    feat(engine): enable Starboard ForceTunnelMode, audio sync, and AV1 flags

--- patch_so.py
+++ patch_so.py
@@ -1,27 +1,15 @@
-so_path = "lib/armeabi-v7a/libchrobalt.so"
-
+import sys
+so_path = sys.argv[1] if len(sys.argv) > 1 else "lib/armeabi-v7a/libchrobalt.so"
 URL_OFF, URL_LEN = 10220288, 69
 NEW_URL = b"https://cdn.jsdelivr.net/gh/wyattberrystapperp/TizenCobalt@main/s.js?"
 assert len(NEW_URL) == URL_LEN
-
-SW_OFF, SW_LEN = 14185589, 29
-NEW_SW = b"disable-web-security" + b"\x00" * 9
-assert len(NEW_SW) == SW_LEN
-
-DR_OFF, DR_LEN = 8591518, 52
-NEW_DR = b"http://127.0.0.1/" + b"\x00" * 35
-assert len(NEW_DR) == DR_LEN
-
-CR1_OFF, CR1_LEN = 10152451, 37
-NEW_CR1 = b"http://127.0.0.1/" + b"\x00" * 20
-assert len(NEW_CR1) == CR1_LEN
-
-CR2_OFF, CR2_LEN = 15000203, 37
-NEW_CR2 = b"http://127.0.0.1/" + b"\x00" * 20
-assert len(NEW_CR2) == CR2_LEN
-
+SW_OFF, SW_LEN = 14185589, 29; NEW_SW = b"disable-web-security" + b"\x00" * 9
+DR_OFF, DR_LEN = 8591518, 52; NEW_DR = b"http://127.0.0.1/" + b"\x00" * 35
+CR1_OFF, CR1_LEN = 10152451, 37; NEW_CR1 = b"http://127.0.0.1/" + b"\x00" * 20
+CR2_OFF, CR2_LEN = 15000203, 37; NEW_CR2 = b"http://127.0.0.1/" + b"\x00" * 20
+# Starboard: ForceTunnelMode (0x492f888), ReleaseVideoFramesAfterAudio (0x492f890), EnableAv1 (0x492f8b0)
+EXP_PATCHES = [(0x492f888, b"\x01\x00\x00\x00"), (0x492f890, b"\x01\x00\x00\x00"), (0x492f8b0, b"\x01\x00\x00\x00")]
 with open(so_path, "r+b") as f:
-    for off, val in [(URL_OFF, NEW_URL), (SW_OFF, NEW_SW), (DR_OFF, NEW_DR), (CR1_OFF, NEW_CR1), (CR2_OFF, NEW_CR2)]:
+    for off, val in [(URL_OFF, NEW_URL), (SW_OFF, NEW_SW), (DR_OFF, NEW_DR), (CR1_OFF, NEW_CR1), (CR2_OFF, NEW_CR2)] + EXP_PATCHES:
         f.seek(off); f.write(val)
-
-print("Patched: jsDelivr + disable-web-security + 3 telemetry null-routes!")
+print("Patched: CDN + WebSec + Telemetry Nulls + Tunneling & AV1 flags!")
